5311f2a1e7904964ebe4f090d3dc8870e38ec8b3
Venegas <dvenegas7@cps.k12.il.us>
2024-12-16 11:43:05 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+# Set the title of the web page
+st.title("Simple Web form with Streamlit")
+
+# Set a header
+st.header("User Information Users")
+
+# Text input for name
+name = st.text_input("enter your name:")
+
+# Dropdown menu for selecting an option
+options = ["Option 1", "Option 2", "Option 3"]
+selected_option = st.selectbox("Choose and Option:", options)
+
+# Slider for selecting a value
+slider_value = st.slider("Select a value", 1 , 100, 50)
+# Submit button
+if st.button("Submit"):
+    st.write(f"Name: {name}")
+    st.write(f"Selected Option: {selected_option}")
+    st.write(f"slider value: {slider_value}")
+# Additional information
+
+# Footer
